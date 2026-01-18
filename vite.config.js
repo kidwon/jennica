d0e2808528bf75fb8f33284792fad1729a5506f5
kidwon <kidwon@gmail.com>
2026-01-18 16:45:45 +0900

    ci: deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGitHubPages && repoName ? `/${repoName}/` : '/',
   plugins: [vue()],
 })
